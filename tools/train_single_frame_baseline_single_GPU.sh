3db9d60863ecffa93d0f850bd1efe2174eec31e2
junhyung-SPALab <junhyunglee@spa.hanyang.ac.kr>
2022-10-21 03:07:25 +0900

    Init code release 22.10.21

--- tools/train_single_frame_baseline_single_GPU.sh
+++ tools/train_single_frame_baseline_single_GPU.sh
@@ -1,4 +1,4 @@
 # Train a PointPillars model
 # ex) python3 train.py --cfg_file cfgs/nuscenes_models/cbgs_pp_multihead.yaml --batch_size 4
-python3 train.py --cfg_file cfgs/nuscenes_models/cbgs_pp_multihead.yaml --batch_size 4
-# python3 train.py --cfg_file {Your config file path} --batch_size {Mini batch_size}
+
+python3 train.py --cfg_file {Your config file path} --batch_size {Mini batch_size}
